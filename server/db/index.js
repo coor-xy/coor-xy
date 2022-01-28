e0396b9bdd3ad594e3655b97c9e6aca7a93d3794
Danny Pacheco <pacheco4@buffalo.edu>
2022-01-28 10:45:15 -0500

    created chart component and db models updated seed

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,16 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Chart = require('./models/Chart')
 
 //associations could go here!
+Chart.belongsTo(User)
+User.hasMany(Chart)
 
 module.exports = {
   db,
   models: {
     User,
+    Chart
   },
 }
