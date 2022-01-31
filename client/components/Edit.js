d616149340e6302d6f27b020113766fc43702978
Blaine Interrante <interrante.blaine@gmail.com>
2022-01-31 17:31:24 -0500

    feat(edit component): adds control form to accept user inputs to their chart's configuration

--- client/components/Edit.js
+++ client/components/Edit.js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from "react";
 import { getRandomColor } from "../utility";
 
 const TESTvalueColumns = ["Group A", "Group B", "Group C", "Group D"];
+const TESTavailableCharts = ["Bar", "Funnel", "Line", "Pie", "Area", "Scatter"];
 
 const Edit = () => {
-  const valueColumns = TESTvalueColumns; // should get from props or redux store
+  const valueColumns = TESTvalueColumns; // we should get columns from props or redux store
+  const availableCharts = TESTavailableCharts; // we should get columns from props or redux store
   const [chartConfig, setChartConfig] = useState({
+    type: "Bar",
     width: 500,
     height: 400,
-    seriesColor: {},
+    seriesColors: [],
   });
 
   useEffect(() => {
-    const mapSeriesColors = valueColumns.reduce((a, col) => {
-      a[col] = getRandomColor();
-      return a;
-    }, {});
-    setChartConfig({ ...chartConfig, seriesColor: {...mapSeriesColors} });
+    // We can fetch a user's saved chart config in this hook if it exists
+    // Otherwise, we can set the chart config in state with some defaults
+    if (!chartConfig.seriesColors.length) {
+      const mapRandomColors = valueColumns.map((col) => ({
+        name: col,
+        color: getRandomColor(),
+      }));
+      setChartConfig({ ...chartConfig, seriesColors: [...mapRandomColors] });
+    }
   }, []);
 
   const handleConfigChange = (e) => {
@@ -30,8 +37,16 @@ const Edit = () => {
     }
   };
 
+  const handleSeriesColorChange = (e) => {
+    const { seriesColors } = chartConfig;
+    seriesColors[e.target.name].color = e.target.value;
+    setChartConfig({ ...chartConfig, seriesColors });
+  };
+
   const handleConfigSubmit = (e) => {
     e.preventDefault();
+    // if new chart, dispatch thunk with post request to add chartConfig to chart model
+    // if existing chart, dispatch thunk with put request to update instance in chart model
   };
 
   return (
@@ -42,25 +57,61 @@ const Edit = () => {
       </div>
       <div>
         <form onSubmit={handleConfigSubmit}>
-          <label htmlFor="width">
-            <small>Width</small>
-          </label>
-          <input
-            name="width"
-            type="number"
-            value={chartConfig.width}
-            onChange={handleConfigChange}
-          />
+          <div>
+            <label htmlFor="type">
+              <small>Chart Type</small>
+            </label>
+            <select
+              name="type"
+              value={chartConfig.type}
+              onChange={handleConfigChange}
+            >
+              {availableCharts.map((type, i) => (
+                <option key={i} value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+          </div>
 
-          <label htmlFor="height">
-            <small>Height</small>
-          </label>
-          <input
-            name="height"
-            type="number"
-            value={chartConfig.height}
-            onChange={handleConfigChange}
-          />
+          <div>
+            <label htmlFor="width">
+              <small>Width</small>
+            </label>
+            <input
+              name="width"
+              type="number"
+              value={chartConfig.width}
+              onChange={handleConfigChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="height">
+              <small>Height</small>
+            </label>
+            <input
+              name="height"
+              type="number"
+              value={chartConfig.height}
+              onChange={handleConfigChange}
+            />
+          </div>
+
+          {chartConfig.seriesColors.map((series, index) => (
+            <div key={index}>
+              <label htmlFor={series.name}>
+                <small>{`${series.name} color`}</small>
+              </label>
+              <input
+                name={index}
+                type="color"
+                value={series.color}
+                onChange={handleSeriesColorChange}
+              />
+            </div>
+          ))}
+
           <button type="submit">Save Changes</button>
         </form>
         {console.log(chartConfig)}
