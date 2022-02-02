778f471ece0a5618152afe6cd1de994800a6abca
Blaine Interrante <interrante.blaine@gmail.com>
2022-02-02 12:01:13 -0500

    feat(charts): adds chart selections to create and edit components

--- client/components/chartComponents/LineComp.js
+++ client/components/chartComponents/LineComp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   LineChart,
   Line,
@@ -7,60 +7,16 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer,
-} from 'recharts';
+} from "recharts";
+import { transformData } from "../../utility";
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
-const LineComp = () => {
+const LineComp = (props) => {
+  const { data, primaryColumn, valueColumns } = props;
   return (
     <LineChart
       width={500}
       height={300}
-      data={data}
+      data={transformData(data, primaryColumn, valueColumns)}
       margin={{
         top: 5,
         right: 30,
@@ -68,18 +24,14 @@ const LineComp = () => {
         bottom: 5,
       }}
     >
-      <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey='name' />
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey={primaryColumn} />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Line
-        type='monotone'
-        dataKey='pv'
-        stroke='#8884d8'
-        activeDot={{ r: 8 }}
-      />
-      <Line type='monotone' dataKey='uv' stroke='#82ca9d' />
+      {valueColumns.map((col, i) => (
+        <Line key={i} type="monotone" dataKey={col.name} stroke={col.color} />
+      ))}
     </LineChart>
   );
 };
--- client/components/chartComponents/SimpleAreaComp.js
+++ client/components/chartComponents/SimpleAreaComp.js
@@ -6,59 +6,16 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
 } from 'recharts';
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { transformData } from "../../utility";
 
-const SimpleAreaComp = () => {
+const SimpleAreaComp = (props) => {
+  const { data, primaryColumn, valueColumns } = props;
   return (
     <AreaChart
       width={500}
       height={400}
-      data={data}
+      data={transformData(data, primaryColumn, valueColumns)}
       margin={{
         top: 10,
         right: 30,
@@ -67,10 +24,12 @@ const SimpleAreaComp = () => {
       }}
     >
       <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey='name' />
+      <XAxis dataKey={primaryColumn} />
       <YAxis />
       <Tooltip />
-      <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
+      {valueColumns.map((col, i) => (
+        <Area key={i} type="monotone" dataKey={col.name} stroke={col.color} fill={col.color}/>
+      ))}
     </AreaChart>
   );
 };
