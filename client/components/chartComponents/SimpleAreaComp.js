1532f80373c317a954fc1d8a5a6be5cb4c2c6568
Blaine Interrante <interrante.blaine@gmail.com>
2022-02-10 12:06:08 -0500

    adds chart download feature: users can download their chart as a png file by clicking a download button on their edit page

--- client/components/chartComponents/SimpleAreaComp.js
+++ client/components/chartComponents/SimpleAreaComp.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback, useEffect } from "react";
+import FileSaver from "file-saver";
+import { useCurrentPng } from "recharts-to-png";
 import {
   AreaChart,
   Area,
@@ -6,42 +8,83 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend
-} from 'recharts';
+  Legend,
+} from "recharts";
 import { transformData } from "../../utility";
 
 const SimpleAreaComp = (props) => {
-  const { data, primaryColumn, valueColumns, width, height, xLabel, yLabel, legend, title, grid,} = props;
+  const {
+    data,
+    primaryColumn,
+    valueColumns,
+    width,
+    height,
+    xLabel,
+    yLabel,
+    legend,
+    title,
+    grid,
+    download,
+  } = props;
+  const [getChartPng, { ref: chartRef }] = useCurrentPng();
+
+  const handleChartDownload = useCallback(async () => {
+    const png = await getChartPng();
+    if (png) {
+      FileSaver.saveAs(png, "area-chart.png");
+    }
+  }, [getChartPng]);
+
+  useEffect(() => {
+    if (download) {
+      handleChartDownload();
+    }
+  }, [download]);
 
   return (
-    <AreaChart
-      width={width}
-      height={height}
-      data={transformData(data, primaryColumn, valueColumns)}
-      margin={{
-        top: 25,
-        right: 30,
-        left: 30,
-        bottom: 35,
-      }}
-    >
-      <text x={(width+60) / 2} y={10} fill="black" textAnchor="middle" dominantBaseline="central">
-        <tspan fontSize="20" fontWeight="bold">{title}</tspan>
-      </text>
-      {grid ? 
-      <CartesianGrid strokeDasharray="3 3"/> :
-      <></>}
-      <XAxis dataKey={primaryColumn} label={{value: xLabel, offset:10, position:"bottom"}}/>
-      <YAxis label={{ value: yLabel, angle: -90, position: 'left' }} />
-      <Tooltip />
-      {legend ?
-      <Legend verticalAlign="top" height={36}/> :
-        <></>
-      }
-      {valueColumns.map((col, i) => (
-        <Area key={i} type="monotone" dataKey={col.name} stroke={col.color} fill={col.color}/>
-      ))}
-    </AreaChart>
+    <div>
+      <AreaChart
+        width={width}
+        height={height}
+        data={transformData(data, primaryColumn, valueColumns)}
+        margin={{
+          top: 25,
+          right: 30,
+          left: 30,
+          bottom: 35,
+        }}
+        ref={chartRef}
+      >
+        <text
+          x={(width + 60) / 2}
+          y={10}
+          fill="black"
+          textAnchor="middle"
+          dominantBaseline="central"
+        >
+          <tspan fontSize="20" fontWeight="bold">
+            {title}
+          </tspan>
+        </text>
+        {grid ? <CartesianGrid strokeDasharray="3 3" /> : <></>}
+        <XAxis
+          dataKey={primaryColumn}
+          label={{ value: xLabel, offset: 10, position: "bottom" }}
+        />
+        <YAxis label={{ value: yLabel, angle: -90, position: "left" }} />
+        <Tooltip />
+        {legend ? <Legend verticalAlign="top" height={36} /> : <></>}
+        {valueColumns.map((col, i) => (
+          <Area
+            key={i}
+            type="monotone"
+            dataKey={col.name}
+            stroke={col.color}
+            fill={col.color}
+          />
+        ))}
+      </AreaChart>
+    </div>
   );
 };
 
--- client/components/chartComponents/StackedBarComp.js
+++ client/components/chartComponents/StackedBarComp.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import FileSaver from "file-saver";
+import { useCurrentPng } from "recharts-to-png";
 import {
   BarChart,
   Bar,
@@ -7,42 +9,84 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  Label
+  Label,
 } from "recharts";
 import { transformData } from "../../utility";
 
 const StackedBarComp = (props) => {
-  const { data, primaryColumn, valueColumns, width, height, xLabel, yLabel, legend, title, grid,} = props;
+  const {
+    data,
+    primaryColumn,
+    valueColumns,
+    width,
+    height,
+    xLabel,
+    yLabel,
+    legend,
+    title,
+    grid,
+    download,
+  } = props;
+
+  const [getChartPng, { ref: chartRef }] = useCurrentPng();
+
+  const handleChartDownload = useCallback(async () => {
+    const png = await getChartPng();
+    if (png) {
+      FileSaver.saveAs(png, "stacked-bar-chart.png");
+    }
+  }, [getChartPng]);
+
+  useEffect(() => {
+    if (download) {
+      handleChartDownload();
+    }
+  }, [download]);
 
   return (
-    <BarChart
-      width={width}
-      height={height}
-      data={transformData(data, primaryColumn, valueColumns)}
-      margin={{
-        top: 25,
-        right: 30,
-        left: 30,
-        bottom: 35,
-      }}
-    >
-      <text x={(width+60) / 2} y={10} fill="black" textAnchor="middle" dominantBaseline="central">
-        <tspan fontSize="20" fontWeight="bold">{title}</tspan>
-      </text>
-      {grid ? 
-      <CartesianGrid strokeDasharray="3 3"/> :
-      <></>}
-      <XAxis dataKey={primaryColumn} label={{value: xLabel, offset:10, position:"bottom"}}/>
-      <YAxis label={{ value: yLabel, angle: -90, position: 'left' }} />
-      <Tooltip />
-      {legend ?
-      <Legend verticalAlign="top" height={36}/> :
-        <></>
-      }
-      {valueColumns.map((col, i) => (
-        <Bar key={i} type="monotone" stackId='1' dataKey={col.name} fill={col.color} />
-      ))}
-    </BarChart>
+    <div>
+      <BarChart
+        width={width}
+        height={height}
+        data={transformData(data, primaryColumn, valueColumns)}
+        margin={{
+          top: 25,
+          right: 30,
+          left: 30,
+          bottom: 35,
+        }}
+        ref={chartRef}
+      >
+        <text
+          x={(width + 60) / 2}
+          y={10}
+          fill="black"
+          textAnchor="middle"
+          dominantBaseline="central"
+        >
+          <tspan fontSize="20" fontWeight="bold">
+            {title}
+          </tspan>
+        </text>
+        {grid ? <CartesianGrid strokeDasharray="3 3" /> : <></>}
+        <XAxis
+          dataKey={primaryColumn}
+          label={{ value: xLabel, offset: 10, position: "bottom" }}
+        />
+        <YAxis label={{ value: yLabel, angle: -90, position: "left" }} />
+        <Tooltip />
+        {legend ? <Legend verticalAlign="top" height={36} /> : <></>}
+        {valueColumns.map((col, i) => (
+          <Bar
+            key={i}
+            type="monotone"
+            stackId="1"
+            dataKey={col.name}
+            fill={col.color}
+          />
+        ))}
+      </BarChart>
+    </div>
   );
 };
 
